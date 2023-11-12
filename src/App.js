cd684215f1a3d5b51d1560b13546c880ba53988c
sayedws <sayedwissem873@gmail.com>
2023-11-12 16:45:54 +0100

    added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  // useEffect(() => {
+  //   const getUsers =() => {
+  //     fetch ('https://jsonplaceholder.typicode.com/users')
+  //     .then((res)=> res.json())
+  //     .then(data=>setUsers(data))
+  //     .catch(err=>console.log(err));
+
+  //   };
+  //   getUsers();
+  //   setLoading(false);
+  //   console.log(users);
+  // }, [])
+  
+
+useEffect(() => {
+  const fetchUsers = async()=>{
+    try {
+      const res = await axios.get("https://jsonplaceholder.typicode.com/users");
+      console.log(res);
+      setUsers(res.data)
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  fetchUsers();
+  setLoading(false);
+
+
+
+}, []);
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">{ loading ? <h1>loading...</h1> : (
+    users.map(el=>(
+      <div>
+    
+      <h3>{el.name}</h3>
+      <h4>{el.adress}</h4>
+      <h5>{el.email}</h5>
+      </div>
+      ))
+    )}
     </div>
   );
 }
